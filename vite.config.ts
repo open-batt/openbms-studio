ac9516ef16bbd75b9465d6835bbc3895d6e809c8
Aleksandar Rajkovic <arjkvc@gmail.com>
2026-05-02 22:30:50 +0100

    chore: add Vitest for React unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
+/// <reference types="vitest" />
 import path from "path"
 
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: "node",
+    globals: true,
+  },
 
   resolve: {
     alias: {
